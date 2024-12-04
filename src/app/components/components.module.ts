c4e06d46e780c52e06333139ded8359dbaa1d95c
GalloGabriel <gabriel.gallo2242@gmail.com>
2024-12-04 18:40:25 -0300

    create filter component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from "@angular/core";
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from "@angular/common";
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AngularMaterialModule } from "../angular-material/angular-material.module";
+
 import { UserDetailsComponent } from './user-details/user-details.component';
+import { FilterComponent } from './filter/filter.component';
+
 
 @NgModule({
   declarations: [
-    UserDetailsComponent
+    UserDetailsComponent,
+    FilterComponent
   ],
   imports: [
-    AngularMaterialModule
+    FormsModule,
+    CommonModule,
+    AngularMaterialModule,
+    BrowserAnimationsModule
   ],
   exports: [
-    UserDetailsComponent
+    UserDetailsComponent,
+    FilterComponent
   ],
 })
 export class ComponentsModule { }
